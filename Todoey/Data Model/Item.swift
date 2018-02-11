379708fbe4e4f101b28664222f2ef67f2657fd3e
Alan Coman <alan@alan-mac-pro.home>
2018-02-11 09:09:04 -0500

    Convert to MVC Design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Alan Coman on 2/9/18.
+//  Copyright © 2018 Alan Coman. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false 
+}
